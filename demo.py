d174cd851c68ddb86b540b317a88fd0c08a37262
Maik Paixão <paixomaik5@gmail.com>
2021-06-02 08:07:08 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -44,7 +44,8 @@ def saveImage(image, path):
 	plt.gca().set_axis_off()
 	plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
 	plt.margins(0,0)
-	plt.imshow(image/255.)
+	plt.autoscale(tight=True)
+	plt.plot(image/255.)
 	plt.savefig(path, bbox_inches='tight')
 
 def main(args):
