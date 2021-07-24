f3ce1b343d060927fc867779bfb4a9d518d7d90f
Maik Luiz Paixão <paixomaik@outlook.com.br>
2021-07-24 20:14:39 -0300

    download.sh fixed

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 
-wget -O './pretrained_r3d/' https://www.dropbox.com/s/bch1gkmppegdlpr/checkpoint?dl=1
-wget -O './pretrained_r3d/' https://www.dropbox.com/s/w99qhttrz1vogxr/pretrained_r3d.data-00000-of-00001?dl=1
-wget -O './pretrained_r3d/' https://www.dropbox.com/s/ji062md266y80w4/pretrained_r3d.index?dl=1
-wget -O './pretrained_r3d/' https://www.dropbox.com/s/jqd4ka0qhs58ms5/pretrained_r3d.meta?dl=1
+wget -P './pretrained_r3d/' https://www.dropbox.com/s/bch1gkmppegdlpr/checkpoint?dl=1
+wget -P './pretrained_r3d/' https://www.dropbox.com/s/w99qhttrz1vogxr/pretrained_r3d.data-00000-of-00001?dl=1
+wget -P './pretrained_r3d/' https://www.dropbox.com/s/ji062md266y80w4/pretrained_r3d.index?dl=1
+wget -P './pretrained_r3d/' https://www.dropbox.com/s/jqd4ka0qhs58ms5/pretrained_r3d.meta?dl=1
 
-wget -O './h5_models/' https://www.dropbox.com/s/v5c8f1ysj48k6ev/doors_model.h5?dl=1
-wget -O './h5_models/' https://www.dropbox.com/s/bc005ii8sg8j70k/windows_model.h5?dl=1
+wget -P './h5_models/' https://www.dropbox.com/s/v5c8f1ysj48k6ev/doors_model.h5?dl=1
+wget -P './h5_models/' https://www.dropbox.com/s/bc005ii8sg8j70k/windows_model.h5?dl=1
