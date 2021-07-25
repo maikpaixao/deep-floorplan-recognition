cf0ee8029a0287f54f30e2664c7028af3d312e97
Maik Luiz Paixão <paixomaik@outlook.com.br>
2021-07-25 15:42:58 -0300

    install script updated

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 
 #install python packages
-python uninstall -r ./scripts/old_versions.txt
-python install -r ./scripts/requirements.txt
+python uninstall -r /scripts/old_versions.txt
+python install -r /scripts/requirements.txt
 
 #install mrcnn
 !chmod +x ./scripts/install_mrcnn.sh
