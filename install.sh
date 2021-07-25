b6c593e19c843b2617ce253546f59b6e47b59ea2
Maik Luiz Paixão <paixomaik@outlook.com.br>
2021-07-25 14:16:21 -0300

    scripts refactored

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+#install python packages
+python uninstall -r ./scripts/old_versions.txt
+python install -r ./scripts/requirements.txt
+
+#install mrcnn
+!chmod +x ./scripts/install_mrcnn.sh
+sh ./scripts/install_mrcnn.sh
